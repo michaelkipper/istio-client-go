db3484d2287f9d3dc95f7c398a32ad440061baf6
Steven Dake <steven.dake@netapp.com>
2019-04-23 13:41:36 -0700

    Add EnvoyFilter type to the API

--- pkg/apis/networking/v1alpha3/register.go
+++ pkg/apis/networking/v1alpha3/register.go
@@ -54,6 +54,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&SidecarList{},
 		&ServiceEntry{},
 		&ServiceEntryList{},
+		&EnvoyFilter{},
+		&EnvoyFilterList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
